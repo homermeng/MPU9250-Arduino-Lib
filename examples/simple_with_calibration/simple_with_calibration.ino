2f88e72d5297d50c7358dbd24f4bf103ea39ab87
homermeng <homer.fansheng.meng@gmail.com>
2024-04-25 10:57:33 +0800

    added print methods in format that is compitable with downstream visualization code.

--- examples/simple_with_calibration/simple_with_calibration.ino
+++ examples/simple_with_calibration/simple_with_calibration.ino
@@ -1,9 +1,12 @@
 #include "MPU9250.h"
 
 MPU9250 mpu;
+const int ledPin = 13;
+unsigned long startTime = 0;
 
 void setup() {
     Serial.begin(115200);
+    pinMode(ledPin, OUTPUT);
     Wire.begin();
     delay(2000);
 
@@ -28,16 +31,26 @@ void setup() {
 
     print_calibration();
     mpu.verbose(false);
+    startTime = millis();
 }
 
 void loop() {
     if (mpu.update()) {
+      /*if (millis() - startTime > 3000) {
+      Serial.println("Data collected for 3 seconds. Stopped.");
+      while (true) { // This will stop the loop from continuing
+          digitalWrite(ledPin, LOW);    // set the LED off
+        }
+      }*/  
+      
         static uint32_t prev_ms = millis();
-        if (millis() > prev_ms + 25) {
-            print_roll_pitch_yaw();
+        if (millis() > prev_ms + 5) {  // sampling rate = 200 Hz
+            print_accel_gyro_mag_viz();
+            //print_roll_pitch_yaw_formatted();
             prev_ms = millis();
         }
     }
+    digitalWrite(ledPin, HIGH);   // set the LED on when sampling
 }
 
 void print_roll_pitch_yaw() {
@@ -79,4 +92,34 @@ void print_calibration() {
     Serial.print(", ");
     Serial.print(mpu.getMagScaleZ());
     Serial.println();
+}
+
+
+// print to the format that is compatible with the viz code
+// viz code url: https://github.com/thecountoftuscany/PyTeapot-Quaternion-Euler-cube-rotation/tree/master
+void print_roll_pitch_yaw_formatted() {   
+    Serial.print("y");
+    Serial.print(mpu.getYaw(), 4);
+    Serial.print("yp");
+    Serial.print(mpu.getPitch(), 4);
+    Serial.print("pr");
+    Serial.print(mpu.getRoll(), 4);
+    Serial.println("r");
+}
+
+// raw sensor reads output in format that is compatible with pyqtgraph viz code
+// viz code see: https://github.com/Sanahm/MPU9250-Data-Visualization/tree/master; mpuScrollingPlot.py
+void print_accel_gyro_mag_viz() {
+  //accel
+  Serial.print(mpu.getAccX()); Serial.print("\t");
+  Serial.print(mpu.getAccY()); Serial.print("\t");
+  Serial.print(mpu.getAccZ()); Serial.print("\t");
+  //gyro
+  Serial.print(mpu.getGyroX()); Serial.print("\t");
+  Serial.print(mpu.getGyroY()); Serial.print("\t");
+  Serial.print(mpu.getGyroZ()); Serial.print("\t");
+  //mag
+  Serial.print(mpu.getMagX()); Serial.print("\t");
+  Serial.print(mpu.getMagY()); Serial.print("\t");
+  Serial.print(mpu.getMagZ()); Serial.print("\n"); // new line in each sampling
 }
